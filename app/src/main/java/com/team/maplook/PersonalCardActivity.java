49d1bb36b1df4bc66145d1ae9b402617d292f5f1
eventua11y <524414145@qq.com>
2016-07-21 21:53:14 +0800

    修改

--- app/src/main/java/com/team/maplook/PersonalCardActivity.java
+++ app/src/main/java/com/team/maplook/PersonalCardActivity.java
@@ -2,6 +2,7 @@ package com.team.maplook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class PersonalCardActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class PersonalCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_card);
+
     }
 }
