321bef29715dea3d88ef535232eb8d5f13e7a3c3
eventua11y <524414145@qq.com>
2016-07-22 12:49:40 +0800

    界面修改

--- app/src/main/java/com/team/maplook/HandleActivity.java
+++ app/src/main/java/com/team/maplook/HandleActivity.java
@@ -4,11 +4,11 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StrangerCardActivity extends Activity {
+public class HandleActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stranger_card);
+        setContentView(R.layout.activity_handle);
     }
 }
