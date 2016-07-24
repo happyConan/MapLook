c6cae1d79f22522794011aae7b4a6f6a0bebf1cf
eventua11y <524414145@qq.com>
2016-07-24 18:00:04 +0800

    修改资料——广播的实现

--- app/src/main/java/com/team/maplook/AlterActivity.java
+++ app/src/main/java/com/team/maplook/AlterActivity.java
@@ -1,8 +1,12 @@
 package com.team.maplook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AlterActivity extends Activity {
 
@@ -10,5 +14,31 @@ public class AlterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alter);
+
+        Button btn_alter_save = (Button)findViewById(R.id.btn_alter_save);
+        final EditText id_c_alter_name = (EditText)findViewById(R.id.id_c_alter_name);
+        final EditText id_c_alter_description = (EditText)findViewById(R.id.id_c_alter_description);
+
+        btn_alter_save.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String alter_name = id_c_alter_name.getText().toString();
+                String alter_description = id_c_alter_description.getText().toString();
+
+                Intent intent_name = new Intent();
+                intent_name.setAction("com.team.maplook");
+                Intent intent_description = new Intent();
+                intent_description.setAction("com.jiaoda.maplook");
+
+                intent_name.putExtra("name",alter_name);
+                sendBroadcast(intent_name);
+                intent_description.putExtra("description",alter_description);
+                sendBroadcast(intent_description);
+
+                finish();
+            }
+        });
     }
+
 }
